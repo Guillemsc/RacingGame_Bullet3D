690af9cb616610e022bd0a60f6ceebfce5a98841
Simón Stoyanov Beltrán <simon.stoyanov@hotmail.com>
2016-12-20 21:43:15 +0100

    audio done

--- Physics3D_class5 - solution/ModulePlayer.cpp
+++ Physics3D_class5 - solution/ModulePlayer.cpp
@@ -155,11 +155,9 @@ update_status ModulePlayer::Update(float dt)
 		vehicle->Brake(brake);
 	}
 
-	vehicle->Render();
+	
 
-	//char title[80];
-	//sprintf_s(title, "%.1f Km/h", vehicle->GetKmh());
-	//App->window->SetTitle(title);
+	vehicle->Render();
 
 	return UPDATE_CONTINUE;
 }
